44ee99242893718e1a09b041f9eb4c10c72ef34c
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-06-10 18:39:57 +0530

    Modified file

--- Recursion/String_Palindrome_using_Recursion.py
+++ Recursion/String_Palindrome_using_Recursion.py
@@ -1,33 +1,55 @@
-# method -1 -->
+# # method -1 -->
 
-s = input("Enter a String : ")
-
-
-def palindrome(s, l=0, r=len(s) - 1):
-    if l == r:
-        return
-    elif s[l] == s[r]:
-        return "String is a Palindrome."
-    elif s[l] != s[r]:
-        return "String is not a Palindrome."
-
-    return palindrome(s, l + 1, r - 1)
+# s = input("Enter a String : ")
 
 
-print(palindrome(s))
+# def palindrome(s, l=0, r=len(s) - 1):
+#     if l == r:
+#         return
+#     elif s[l] == s[r]:
+#         return "String is a Palindrome."
+#     elif s[l] != s[r]:
+#         return "String is not a Palindrome."
+
+#     return palindrome(s, l + 1, r - 1)
 
 
-# method -2 -->
-s = input("Enter a String : ")
+# print(palindrome(s))
+
+# -----------------------------------------------------------------------------
+
+# # method -2 -->
+# s = input("Enter a String : ")
 
 
-def palindrome(s, l=0, r=len(s) - 1):
-    if l >= r:
-        return "String is a Palindrome."
-    elif s[l] != s[r]:
-        return "String is not a Palindrome."
+# def palindrome(s, l=0, r=len(s) - 1):
+#     if l >= r:
+#         return "String is a Palindrome."
+#     elif s[l] != s[r]:
+#         return "String is not a Palindrome."
 
-    return palindrome(s, l + 1, r - 1)
+#     return palindrome(s, l + 1, r - 1)
 
 
-print(palindrome(s))
+# print(palindrome(s))
+
+# -----------------------------------------------------------------------------
+
+# method -3 -->
+
+
+def palidrome():
+    s = input("Enter the String :")
+    n = len(s)
+    l = 0
+    r = n - 1
+    while l < r:
+        if s[l] == s[r]:
+            l += 1
+            r -= 1
+        else:
+            return "String is not Palindrome."
+    return "String is Palindrome."
+
+
+print(palidrome())
