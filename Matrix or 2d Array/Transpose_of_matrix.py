c986dec3d8fc89d247d0b256b2245d6b6b4d7da7
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-06-16 16:52:13 +0530

    Modified transpose of matrix file

--- Matrix or 2d Array/Transpose_of_matrix.py
+++ Matrix or 2d Array/Transpose_of_matrix.py
@@ -1,6 +1,7 @@
-matrix = [[0, 1, 2, 3], [3, 4, 5, 6], [6, 7, 8, 9], [9, 0, 1, 2]]
+matrix = [[0, 1, 2, 3], [3, 4, 5, 6], [6, 7, 8, 9]]
 rows = len(matrix)
 col = len(matrix[0])
+
 print("Metrix is : ")
 for i in range(0, rows):
     for j in range(0, col):
@@ -8,10 +9,11 @@ for i in range(0, rows):
     print()
 print("-------")
 print("Transpose of matrix is : ")
-for i in range(0, rows):
-    for j in range(0, col):
+for i in range(0, col):
+    for j in range(0, rows):
         print(matrix[j][i], end=" ")
     print()
 
-T_matrix = [[matrix[j][i] for j in range(0, col)] for i in range(0, rows)]
+# using list comprehension --->
+T_matrix = [[matrix[j][i] for j in range(0, rows)] for i in range(0, col)]
 print(f"\nTranspose matrix is : {T_matrix}")
