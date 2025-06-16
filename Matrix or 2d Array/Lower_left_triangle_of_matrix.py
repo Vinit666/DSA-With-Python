0d1f6b4c1de97d630300f49a1a338c60f5c7a1a9
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-06-16 15:18:28 +0530

    Added lower left triangle of matrix file

---
+++ Matrix or 2d Array/Lower_left_triangle_of_matrix.py
@@ -0,0 +1,12 @@
+# print lower left triangle --->
+
+matrix = [[0, 1, 2, 3], [3, 4, 5, 6], [6, 7, 8, 9], [9, 0, 1, 2]]
+rows = len(matrix)
+col = len(matrix[0])
+
+
+for i in range(0, rows):
+    for j in range(0, col):
+        if i >= j:
+            print(matrix[i][j], end=" ")
+    print()
