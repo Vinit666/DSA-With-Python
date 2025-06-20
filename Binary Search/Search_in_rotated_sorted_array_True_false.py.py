9c9399291d30db9a958eded8271a3496c7b05eb0
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-06-20 15:20:38 +0530

    Added True or False by Search in rotated sorted array file

---
+++ Binary Search/Search_in_rotated_sorted_array_True_false.py.py
@@ -0,0 +1,27 @@
+def search_rsa(nums, target):
+    n = len(nums)
+    low = 0
+    high = n - 1
+    ti = -1
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
+            ti = mid
+            break
+        elif nums[mid] <= nums[high]:
+            if nums[mid] <= target <= nums[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+        else:
+            if nums[low] <= target <= nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+    if ti > 0:
+        return True
+    else:
+        return False
+
+
+print(search_rsa(nums=[17, 18, 20, 1, 3, 4, 5, 7, 10, 11, 16], target=0))
