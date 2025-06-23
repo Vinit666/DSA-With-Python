c2817dbf811a5a4f19972645d4eb57e25fd4db49
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-06-23 11:50:36 +0530

    Modified Check cycle in linked list file

--- Linked List/Singly Linked List/Linked_list_cycle_check.py
+++ Linked List/Singly Linked List/Linked_list_cycle_check.py
@@ -66,7 +66,47 @@
 # print(s1.hasCycle())
 
 
-# method 2 --->
+# # method 2 ---> making cycle usnig node class
+
+
+# class Node:
+#     def __init__(self, val):
+#         self.val = val
+#         self.next = None
+
+
+# class SinglyLinkedList:
+#     def __init__(self):
+#         self.head = None
+
+#     def hasCycle(self):
+#         self.head = n1
+#         slow = self.head
+#         fast = self.head
+
+#         while fast is not None and fast.next is not None:
+#             slow = slow.next
+#             fast = fast.next.next
+#             if slow == fast:
+#                 return True
+#         return False
+
+
+# s1 = SinglyLinkedList()
+# n1 = Node(10)
+# n2 = Node(20)
+# n3 = Node(30)
+# n4 = Node(44)
+# n5 = Node(99)
+# n1.next = n2
+# n2.next = n3
+# n3.next = n4
+# n4.next = n5
+# n5.next = n1  # making cycle here
+# print(s1.hasCycle())
+
+
+# method 3 --->making cycle using makecycle()
 
 
 class Node:
@@ -99,16 +139,14 @@ class SinglyLinkedList:
                 current = current.next
             print()
 
-    def rll(self):
-        cuurent = self.head
-        pre_node = None
-
-        while cuurent is not None:
-            front_node = cuurent.next
-            cuurent.next = pre_node
-            pre_node = cuurent
-            cuurent = front_node
-        self.head = pre_node
+    def make_cycle(self):
+        a = self.head
+        current = self.head
+        while current is not None:
+            current = current.next
+            if current.next is None:
+                current.next = a
+                return
 
     def hasCycle(self):
         slow = self.head
@@ -129,4 +167,7 @@ s1.append(6)
 s1.append(8)
 s1.append(10)
 s1.traversal()
+
+s1.make_cycle()
+
 print(s1.hasCycle())
