9e6e49ed21bb80d3c078931980d8fffa82b39ef8
Vinit666 <128893260+Vinit666@users.noreply.github.com>
2025-07-07 13:19:14 +0530

    Added infix to prefix file

---
+++ Infix Prefix Postfix/Infix_to_prefix.py
@@ -0,0 +1,40 @@
+class Solution:
+    def precedence(self, ch):
+        if ch == "^":
+            return 3
+        elif ch == "*" or ch == "/":
+            return 2
+        elif ch == "+" or ch == "-":
+            return 1
+        else:
+            return 0
+
+    def InfixToPostfix(self, s):
+        s = s[::-1]
+        s = s.replace("(", "temp").replace(")", "(").replace("temp", ")")
+        stack = []
+        result = []
+
+        for char in s:
+            if ("a" <= char <= "z") or ("A" <= char <= "Z") or ("0" <= char <= "9"):
+                result.append(char)
+            elif char == "(":
+                stack.append(char)
+            elif char == ")":
+                while stack and stack[-1] != "(":
+                    result.append(stack.pop())
+                stack.pop()
+            else:
+                while stack and self.precedence(stack[-1]) > self.precedence(char):
+                    result.append(stack.pop())
+                stack.append(char)
+
+        while stack:
+            result.append(stack.pop())
+
+        return "".join(result[::-1])
+
+
+s1 = Solution()
+print(s1.InfixToPostfix("a+b*(c^d-e)"))
+print(s1.InfixToPostfix("(a+b)*c-d+f"))
